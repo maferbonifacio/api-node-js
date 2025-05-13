d3ee188851b9e6199bd134f5eada0fc9093451fe
maferbonifacio <160647200+maferbonifacio@users.noreply.github.com>
2025-05-13 10:30:55 -0300

    13-05

---
+++ src/controllers/feedback_consulta.js
@@ -0,0 +1,66 @@
+const db = require('../dataBase/connection'); 
+
+module.exports = {
+    async listarFeedback_consulta(request, response) {
+        try {
+            const sql = 'select * from feedback_consulta;'
+            const [rows] = await db.query(sql);
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Lista de feedback_consulta', 
+                dados: rows
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async cadastrarFeedback_consulta(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Cadastro de feedback_consulta', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async editarFeedback_consulta(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Alteração no cadastro de feedback_consulta', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async apagarFeedback_consulta(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Exclusão de feedback_consulta', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+};  
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const UsuariosController = require('../controllers/usuarios'); 
 const Perfil_psicologoController = require('../controllers/perfil_psicologo'); 
+const Feedback_consultaController = require('../controllers/feedback_consulta'); 
 
 router.get('/usuarios', UsuariosController.listarUsuarios); 
 router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
@@ -13,6 +14,11 @@ router.get('/perfil_psicologo', Perfil_psicologoController.listarPerfil_psicolog
 router.post('/perfil_psicologo', Perfil_psicologoController.cadastrarPerfil_psicologo); 
 router.patch('/perfil_psicologo', Perfil_psicologoController.editarPerfil_psicologo); 
 router.delete('/perfil_psicologo', Perfil_psicologoController.apagarPerfil_psicologo); 
+
+router.get('/feedback_consulta', Feedback_consultaController.listarFeedback_consulta); 
+router.post('/feedback_consulta', Feedback_consultaController.cadastrarFeedback_consulta); 
+router.patch('/feedback_consulta', Feedback_consultaController.editarFeedback_consulta); 
+router.delete('/feedback_consulta', Feedback_consultaController.apagarFeedback_consulta); 
 
 
 module.exports = router;
